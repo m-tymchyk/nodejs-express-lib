cdd4d18f7890ee1824f4e5caa1065f1615349eb1
andriikryshtal <andrew.kryshtal@gmail.com>
2018-03-29 16:14:55 +0300

    small refactor for layouts using handlebars

--- server.js
+++ server.js
@@ -1,21 +1,84 @@
 var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var expressValidator = require("express-validator");
+var flash = require("connect-flash");
+var session = require("express-session");
+var LocalStrategy = require("passport-local").Strategy;
 var MongoClient = require("mongodb").MongoClient;
 var ObjectID = require("mongodb").ObjectID;
 var postsController = require("./controllers/posts");
 var db = require("./db");
 var passport = require("passport");
 
+var routes = require("./routes/index");
+var viewusers = require("./routes/viewusers");
+
 var app = express();
 
+// View Engine
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({ defaultLayout: "layout" }));
+app.set("view engine", "handlebars");
+
+// BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Set Static Folder
 app.use(express.static("."));
 app.use(express.static(__dirname + "/assets"));
 
-app.get("/", function(req, res) {
-  res.render("login.ejs");
+// Express Session
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+  })
+);
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  next();
 });
+
+app.use("/", routes);
+app.use("/viewusers", viewusers);
 
 app.get("/posts", postsController.all);
 
